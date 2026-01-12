77ce10f68e48df1148a1ca340ba447d521ca1bc7
balintedoc <orosz.balint.gm@gmail.com>
2026-01-12 19:11:52 +0000

    Add files via upload

---
+++ Name_Variable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	std::string name = "Balint";
+
+	std::cout << "Hello, My name is ";
+	std::cout << name;
+	std::cout << " and I am learning C++.";
+	return 0;
+}
